05f3bcf38af4112716766f305ec02e2b49a01643
Tom <treilly94@live.com>
2016-09-12 14:19:14 +0000

    jira

--- jira.sh
+++ jira.sh
@@ -1,14 +1,19 @@
+#!/usr/bin/expect
+
 #navigate to the bin folder
 cd /bin/
 #download the file
 sudo wget https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-software-7.2.1-x64.bin
 #check that it is executible
-sudo chmod a + x atlassian-jira-software-7.2.1-x64.bin
+sudo chmod a+x atlassian-jira-software-7.2.1-x64.bin
 #run the file
 sudo ./atlassian-jira-software-7.2.1-x64.bin
+#everything below here doesnt work yet
 #ok to install
-o
+expect "OK [o, Enter], Cancel [c]" {send "o\r"}
 #choose express install
 1
 #install
 i
+
+interact
