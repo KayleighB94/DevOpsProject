97012737640a4818abaf14c1047a9600c5be8da5
Edward <Edward.Stevens@qa.com>
2016-09-12 16:43:37 +0000

    bootstrap master edited

---
+++ Vagrant/bootstrap_master.sh
@@ -0,0 +1,11 @@
+sudo apt-get install –y openssh-server openssh-client
+
+sudo apt-get install –y puppet puppetmaster
+
+sudo ufw disable
+
+#Variables
+agentip = $(sudo facter ipaddress_eth1)
+agentfqdn = $(sudo facter fqdn)
+
+sed -i "1s/^/127.0.0.1 $agentfqdn puppetmaster \n$agentip $agentfqdn puppetmaster\n /" /etc/hosts
