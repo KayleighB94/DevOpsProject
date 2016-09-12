68bd182961cbf9b0d2370c143e32bfacb3cd5882
Tom <treilly94@live.com>
2016-09-12 12:54:45 +0000

    jira 3

--- jira.sh
+++ jira.sh
@@ -1,7 +1,9 @@
-#Find the file
-wget https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-software-7.2.1-x64.bin
+#navigate to the bin folder
+cd /bin/
+#download the file
+sudo wget https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-software-7.2.1-x64.bin
 #check that it is executible
 chmod a + x atlassian-jira-software-7.2.1-x64.bin
 #run the file
 sudo ./atlassian-jira-software-7.2.1-x64.bin
-#choose express install 
+#choose express install
