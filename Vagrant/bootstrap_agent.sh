ead1a86383cfbb4f8cd33558dcf0b86396ee98d7
Tom <treilly94@live.com>
2016-09-12 16:29:15 +0000

    bootstrap agent

--- Vagrant/bootstrap_agent.sh
+++ Vagrant/bootstrap_agent.sh
@@ -14,3 +14,12 @@ IpId = $(sudo facter ipaddress_eth1)
 
 #edit host file
 sudo vi /etc/hosts
+
+192.168.1.25  ammaster.qac.local  puppetmaster
+127.0.0.1 FqdnId  puppet
+IpId  FqdnId  puppet
+
+#edit puppet conf
+sudo vi /etc/puppet/puppet.conf
+
+server = ammaster.qac.local
