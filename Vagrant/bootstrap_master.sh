797fa60ae6a6e04bdffd2c12c98493b2b974ea85
Edward <Edward.Stevens@qa.com>
2016-09-13 09:44:18 +0000

    Added echos

--- Vagrant/bootstrap_master.sh
+++ Vagrant/bootstrap_master.sh
@@ -5,19 +5,27 @@ sudo apt-get update
 sudo apt-get install -y openssh-server openssh-client
 
 sudo ufw disable
+echo 'devops - ufw disabled'
 
 sudo apt-get install -y puppet puppetmaster
+echo 'devops - puppet installed'
 
 #Variables
 agentfqdn=$(sudo facter fqdn)
 agentip=$(sudo facter ipaddress_eth1)
+echo  'devops - variables set ' $agentfqdn 
+echo  'devops - variables set ' $agentip 
 
 sudo sed -i "1s/^/127.0.0.1 $agentfqdn puppetmaster \n$agentip $agentfqdn puppetmaster\n /" /etc/hosts
+echo 'devops - hosts edited'
 
 sudo touch /etc/puppet/manifests/site.pp
+echo 'devops - site touched'
 
 sudo puppet cert list
+echo 'devops - certs listed'
 sudo puppet cert sign –all
+echo 'devops - certs signed'
 
 #wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 #sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
