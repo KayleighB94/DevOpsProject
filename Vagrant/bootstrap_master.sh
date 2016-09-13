6398d8c8387b40b5cecb73cd9cfa0118aa254f1c
chibz93 <chibuzo.nwobiri@qa.com>
2016-09-13 11:39:10 +0000

    c

--- Vagrant/bootstrap_master.sh
+++ Vagrant/bootstrap_master.sh
@@ -20,4 +20,6 @@ sudo puppet cert sign –all
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get install -y jenkins
+
+sudo apt-get install -y maven
