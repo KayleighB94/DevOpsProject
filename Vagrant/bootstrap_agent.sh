3d6c95dd6c70c94b3982b09fd8af960ab6573dbf
Edward <Edward.Stevens@qa.com>
2016-09-13 14:54:08 +0000

    ed

--- Vagrant/bootstrap_agent.sh
+++ Vagrant/bootstrap_agent.sh
@@ -17,11 +17,11 @@ agentip=$(sudo facter ipaddress_eth1)
 echo 'devops - Variables set'
 
 #edit host file
-sudo sed -i "1s/^/192.168.1.25  ammaster.qac.local  puppetmaster \n127.0.0.1 $agentfqdn puppet \n$agentip $agentfqdn puppet\n\n/" /etc/hosts
+sudo sed -i "1s/^/192.168.1.25  ammaster3.qac.local  puppetmaster \n127.0.0.1 $agentfqdn puppet \n$agentip $agentfqdn puppet\n\n/" /etc/hosts
 echo 'devops - Hostfile edited'
 
 #edit puppet conf
-sudo sed -i "2i server = ammaster.qac.local" /etc/puppet/puppet.conf
+sudo sed -i "2i server = ammaster3.qac.local" /etc/puppet/puppet.conf
 echo 'devops - Puppet.conf edited'
 
 #restart puppet
